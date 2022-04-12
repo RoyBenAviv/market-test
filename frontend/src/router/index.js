5bd1fac60b251ccb2f930c253a81b360ecdfd686
RZShachar <RZShachar@gmail.com>
2022-04-12 14:02:41 +0300

    Opened cmps

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,9 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import addMarketer from '../cmps/add-marketer.vue'
+import appDashboard from '../cmps/app-dashboard.vue'
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
-
+    {
+      path: '/',
+      name: 'home',
+      component: addMarketer
+    },
+    {
+      path: '/dashobard',
+      name: 'dashboard',
+      component: appDashboard
+    }
 
   ]
 })
