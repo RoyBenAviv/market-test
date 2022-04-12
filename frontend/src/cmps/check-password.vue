acd27fe9766506a8f56d401f740686a42ff37199
Tamir <Tamirbelisha@gmail.com>
2022-04-12 15:41:57 +0300

    validate password

---
+++ frontend/src/cmps/check-password.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <h1>Hello, please insert your username and password</h1>
+    <input v-model="user.username" type="text" name="" id="" placeholder="Username" />
+    <input v-model="user.password" type="password" name="" id="" placeholder="Password" />
+    <button @click="checkPassword">Submit</button>
+  </section>
+</template>
+
+<script>
+export default {
+  name: '',
+  data() {
+    return {
+      user: {
+        username: '',
+        password: '',
+      },
+    }
+  },
+  methods: {
+    checkPassword() {
+      if (this.user.username === 'Admin' && this.user.password === '1111') this.$emit('checked')
+    },
+  },
+  computed: {},
+  components: {},
+}
+</script>
+
+<style></style>
