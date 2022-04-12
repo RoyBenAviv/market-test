329d481a1eb8f2464256ec59beaf241ec1a71f56
RZShachar <RZShachar@gmail.com>
2022-04-12 14:06:48 +0300

    Another minor fix

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,15 @@
 <template>
-  <task-app />
+  <router-view />
 </template>
 
 <script>
-import taskApp from './views/task-app.vue'
+
 
 export default {
   name: 'App',
   created() {
   },
   components: {
-    taskApp
   }
 }
 
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,13 +1,13 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import addMarketer from '../cmps/add-marketer.vue'
-import appDashboard from '../cmps/app-dashboard.vue'
+import taskApp from '../views/task-app.vue'
+import appDashboard from '../views/app-dashboard.vue'
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: addMarketer
+      component: taskApp
     },
     {
       path: '/dashobard',
