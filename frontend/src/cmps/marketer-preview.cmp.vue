0cf24431ad85b1646347ad5e4540cf0ca8823d4f
Roee Furman <roeefurman1@gmail.com>
2022-04-12 15:18:31 +0300

    commit both folders

--- frontend/src/cmps/marketer-preview.cmp.vue
+++ frontend/src/cmps/marketer-preview.cmp.vue
@@ -1,14 +1,15 @@
 <template>
   <section class="marketer-preview">
+    {{ marketer }}
     <tr>
-      <td>{{ marketer.firstName }}</td>
-      <td>{{ marketer.lastName }}</td>
-      <td>{{ marketer.email }}</td>
-      <td>{{ marketer.website }}</td>
-      <td>{{ marketer.linkedin }}</td>
-      <td>{{ marketer.experience }}</td>
-      <td>{{ marketer.budget }}</td>
-      <td>{{ marketer.createdAt }}</td>
+      <td>{{ marketer?.firstName }}</td>
+      <td>{{ marketer?.lastName }}</td>
+      <td>{{ marketer?.email }}</td>
+      <td>{{ marketer?.website }}</td>
+      <td>{{ marketer?.linkedin }}</td>
+      <td>{{ marketer?.experience }}</td>
+      <td>{{ marketer?.budget }}</td>
+      <td>{{ marketer?.createdAt }}</td>
     </tr>
   </section>
 </template>
