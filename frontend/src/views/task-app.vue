6afe1935f8f11f57bae9faa70c6dcc63207c185c
Roy Ben Aviv <roybenavivv@gmail.com>
2022-04-12 15:49:03 +0300

    style task-app

--- frontend/src/views/task-app.vue
+++ frontend/src/views/task-app.vue
@@ -6,21 +6,21 @@
             <label>First name
                 <input v-model="marketer.firstName" type="text">
             </label>
-            <label>Last name
+            <label>Last name <span class="req">*</span>
                 <input v-model="marketer.lastName" type="text">
             </label>
             <label>Email address
                 <input v-model="marketer.email" type="email" required>
             </label>
-            <label>Website add
+            <label>Website
                 <input v-model="marketer.website" type="text">
             </label>
             <label>Linkedin
                 <input v-model="marketer.linkedin" type="text">
             </label>
             <div>
-                <span>How many years of exprience do you have with Facebook Marketing?</span>
-                <div>
+                <p>* How many years of exprience do you have with Facebook Marketing?</p>
+                <div class="years-options">
                 <label v-for="data in exprienceData" :key="data">
                     <input v-model="marketer.exprience" type="radio" :value="data">
                     {{ data }}
@@ -28,12 +28,12 @@
                 </div>
 
             </div>
-            <div>
-                <label>
-                    what was the biggest campaign budget you have managed in a single month?
-                    <input type="range" min="1000" max="500000" v-model="marketer.range">
-                    ${{ range }}
-                </label>
+            <div class="budget">
+                <p>
+                    * what was the biggest campaign budget you have managed in a single month?
+                </p>
+                    <div class="input-range"><input type="range" min="1000" max="500000" v-model="marketer.range">
+                    <span class="range">${{ range }}</span></div>
             </div>
 
             <button>Continue</button>
