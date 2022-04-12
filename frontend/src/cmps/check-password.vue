d80e6ca44a08fb8141887293b2a10a33041903f4
Tamir <Tamirbelisha@gmail.com>
2022-04-12 15:49:57 +0300

    added password check

--- frontend/src/cmps/check-password.vue
+++ frontend/src/cmps/check-password.vue
@@ -1,9 +1,10 @@
 <template>
   <section>
-    <h1>Hello, please insert your username and password</h1>
-    <input v-model="user.username" type="text" name="" id="" placeholder="Username" />
-    <input v-model="user.password" type="password" name="" id="" placeholder="Password" />
+    <h2>Hello, please insert your username and password</h2>
+    <input @keydown.enter="checkPassword" v-model="user.username" type="text" name="" id="" placeholder="Username" required/>
+    <input @keydown.enter="checkPassword" v-model="user.password" type="password" name="" id="" placeholder="Password" required/>
     <button @click="checkPassword">Submit</button>
+    <h3 v-if="wrongAnswer" style="color: red;">*Wrong username or password !</h3>
   </section>
 </template>
 
@@ -16,11 +17,13 @@ export default {
         username: '',
         password: '',
       },
+      wrongAnswer: false
     }
   },
   methods: {
     checkPassword() {
       if (this.user.username === 'Admin' && this.user.password === '1111') this.$emit('checked')
+      else this.wrongAnswer = true
     },
   },
   computed: {},
